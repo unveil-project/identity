5a1f81056dcf920fd63bcf9233b88e3040e738a3
MatteoGabriele <m.gabriele.dev@gmail.com>
2026-06-25 22:15:46 +0200

    feat: add export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { getClassificationDetails } from "./classification";
 export { CONFIG as identityConfig } from "./config";
 export { identify } from "./identify";
 export type {
+	FlagDataPoint,
 	GitHubEvent,
 	GitHubUser,
 	IdentifyResult,
